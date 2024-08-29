c19d13e61f8e8a711dbff368784178d245060b96
Nazeer Ahmad <nazeer.finance20@gmail.com>
2024-08-29 10:59:48 -0400

    Programmed the Add/Remove Button

--- front/src/App.js
+++ front/src/App.js
@@ -6,8 +6,20 @@ import "./App.css"
 
 
 export default function MyApp() {
-  const [input, setlists] = useState([]);
+  const [things, setlists] = useState([]);
   const [inputval, setinputval] =  useState('');
+ 
+  const add_thing = () =>{
+   if(inputval.trim() !== ' '){
+    setlists([...things, inputval]);
+    setinputval('');
+   }
+  }
+   const remove_thing = (index) => {
+    const new_lst = things.filter((_, i) => i !== index);
+      setlists(new_lst);
+  }
+ 
 
   //handleSubmit: function(e){ if (!this.validate()) { e.preventDefault() } }
   //For the John Doe I want to create Profiles in the future
@@ -15,11 +27,26 @@ export default function MyApp() {
   return (
     <div class = "App">
       <h2>Welcome John Doe, Please add to your list</h2>
-      <body>Your List</body>
-      <input type = 'text' value={inputval}/>
-      <button>Add</button>
-      <button>Edit</button>
-      <button>Delete</button>
+      <h3>List 1</h3>
+      <label>
+       
+        <input
+         type= 'text'
+         value={inputval}
+         onChange={(e) => setinputval(e.target.value)}
+        />
+      <button onClick={add_thing}>Add</button>
+      <ul>
+        {things.map((things, index) => (
+          <li key={index}>
+            {things}
+            <button onClick={() => remove_thing(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      
+  
+      </label>
     </div>
   );
-}
+};
