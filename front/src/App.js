684003839c22f9f4a7e2ec9edc2a96fe43b6b23b
Nazeer Ahmad <nazeer.finance20@gmail.com>
2024-08-29 08:53:42 -0400

    Added some placeholders
    I added some placeholders and will remove them. I am thinking of doing a table component.

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,25 @@
 
 import React from 'react'
 import { useState } from 'react'
+import "./App.css"
+
 
 
 export default function MyApp() {
   const [input, setlists] = useState([]);
   const [inputval, setinputval] =  useState('');
 
-  
+  //handleSubmit: function(e){ if (!this.validate()) { e.preventDefault() } }
+  //For the John Doe I want to create Profiles in the future
+  // It will correct to the user's name
   return (
-    <div>
-      <h1>Welcome John Doe, Please add to your list</h1>
-      <input type='text' value={inputval}/>
-      <button>Submit</button>
-      
-    
+    <div class = "App">
+      <h2>Welcome John Doe, Please add to your list</h2>
+      <body>Your List</body>
+      <input type = 'text' value={inputval}/>
+      <button>Add</button>
+      <button>Edit</button>
+      <button>Delete</button>
     </div>
   );
 }
